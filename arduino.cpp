2fdba142d16c69a526aa862f36011ae7880484cb
nvarcha <nvarcha@gmail.com>
2020-04-01 14:51:45 -0300

    Cleaned up

--- arduino.cpp
+++ arduino.cpp
@@ -6,7 +6,6 @@
 Debug debug;
 Board board;
 Ventilator ventilator(&debug, &board);
-//ElectroValve electroValve(&board, "O2 intake", 11, OUTPUT);
 
 /**
  * Arduino setup - runs only once
@@ -26,17 +25,6 @@ void setup() {
 
     // Run diagnostics
     ventilator.runDiagnostics();
-
-    //ventilator.oxigen_intake = ElectroValve(&debug, &board, "O2 intake", 11, OUTPUT);
-    // Configure the following pins as output pins
-    //pinMode(ev1_o2_in, OUTPUT);
-
-    // Configure the following pins as input pins
-    // INPUT_PULLUP inverts the behaviour so HIGH means off and LOW means on
-    //pinMode(pin_start_stop_button, INPUT_PULLUP);
-
-    // Configure interrupt to read on/off button
-    //attachInterrupt(digitalPinToInterrupt(pin_start_stop_button), togglePower, CHANGE);
 }
 
 /**
